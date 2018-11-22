354361545acfdb21a55da10c39d1b22baf125ab0
Hectordeluna <raptor0sp@gmail.com>
2018-11-22 17:37:32 -0600

    Re

--- Taxi/main/views.py
+++ Taxi/main/views.py
@@ -5,34 +5,6 @@ from .models import Boleto
 from django.contrib.auth.models import User
 import config
 
-viajes = [
-    {
-        'fecha' : 'OCT 07 2018 - 8:30 AM',
-        'origen' : 'Tecnologico de Mty',
-        'destino' : 'Estacion Autobus Mty',
-        'costo' : '100.00',
-    },
-    {
-        'fecha' : 'OCT 10 2018 - 12:30 PM',
-        'origen' : 'Soriana Centro Saltillo',
-        'destino' : 'Estacion Autobus Saltillo',
-        'costo' : '90.00',
-    },
-    {
-        'fecha' : 'OCT 15 2018 - 10:30 PM',
-        'origen' : 'Estacion San Luis Potosi',
-        'destino' : 'Calle Mirador, #240, Col.',
-        'costo' : '54.00',
-    }
-]
-
-user = {
-    'nombre' : 'Hector',
-    'apellido' : 'De Luna',
-    'edad' : 21,
-    'viajes' : 341,
-}
-
 # Create your views here.
 def home(request):
     username = None
