e593372e4851b0e480a09d03c147791ae49f746d
Hectordeluna <raptor0sp@gmail.com>
2018-11-26 16:59:03 -0600

    Encuestas

--- Taxi/main/forms.py
+++ Taxi/main/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Encuesta
+from datetime import datetime
+
 
 RESPUESTAS= (
     ('bueno', 'Bueno'),
@@ -8,10 +11,19 @@ RESPUESTAS= (
     ('malo', 'Malo'),
 )
 
-class TaxiForm(forms.Form):
+class TaxiForm(forms.ModelForm):
+    user_fk = forms.IntegerField(required=False,widget=forms.HiddenInput())
+    taxi_fk = forms.IntegerField(required=False,widget=forms.HiddenInput())
+    promedio = forms.DecimalField(required=False,decimal_places=2,widget=forms.HiddenInput())
+    fecha = forms.DateTimeField(initial=datetime.now(), required=False,widget=forms.HiddenInput())
     pregunta1= forms.CharField(label='¿Como califica el tiempo de llegada del servicio?', widget=forms.Select(choices=RESPUESTAS))
     pregunta2= forms.CharField(label='¿Como califica la atencion del conductor?', widget=forms.Select(choices=RESPUESTAS))
     pregunta3= forms.CharField(label='¿Como califica la limpieza del vehículo?', widget=forms.Select(choices=RESPUESTAS))
+
+    class Meta:
+        model = Encuesta
+        fields = ('pregunta1','pregunta2','pregunta3', 'user_fk', 'taxi_fk','fecha','promedio',)
+        widgets = {'user_fk': forms.HiddenInput(), 'taxi_fk' : forms.HiddenInput()}
 
 class UserRegisterForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True)
