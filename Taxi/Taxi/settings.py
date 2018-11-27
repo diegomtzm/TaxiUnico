c6d9995f0d85b4df1ad534999dc68e361b8bae3f
ingridhdezbenz <ingridhdezbenz@hotmail.com>
2018-11-26 22:10:52 -0600

    Recuperar contraseña
    recuperar contraseña

--- Taxi/Taxi/settings.py
+++ Taxi/Taxi/settings.py
@@ -133,3 +133,10 @@ STATICFILES_DIRS = [
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ingridhdezbenz@gmail.com'
+EMAIL_HOST_PASSWORD = 'KonstanZ,0,7'
